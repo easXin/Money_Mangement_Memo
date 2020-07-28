e01dfa7fe7551fc20647c3f8a72abff3e7db01cc
easXin <codingericwang@gmail.com>
2020-07-28 13:31:37 -0700

    added gravatar to handle user profile img

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,8 @@ const router = express.Router();
 const User = require("../../db/User");
 // use this lib to encrypt passcode
 const bcrypt = require("bcrypt");
+// for user profile avatar
+const gravatar = require("gravatar");
 // define router
 router.get("/test", (req, res) => {
     res.json({msg:"login works"});
@@ -20,13 +22,16 @@ router.post("/register",(req,res) => {
             if(user){
                 return res.status(400).json({email:"This email has been taken"})
             }else{
+                // 1st arg: target  => s: size , r: format, d : default {404 ; err, mm: default profile img}
+                let avatar = gravatar.url(req.body.email,{s:'200',r:'pg',d:'mm'});
                 // create new user
                 const newUser = new User({
                     name: req.body.name,
                     email: req.body.email,
                     password: req.body.password,
+                    avatar,
                 });
-                bcrypt.genSalt(10, function (err, salt) {
+                bcrypt.genSalt(10, (err, salt) => {
                     // 1st arg: target, 2nd arg: encrypt alg
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
                         if(err)
