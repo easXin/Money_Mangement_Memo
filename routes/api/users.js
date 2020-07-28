54459fd30a3309832482a69504578a94009d71eb
easXin <codingericwang@gmail.com>
2020-07-28 11:50:09 -0700

    set up register end, encrypted user passcode with the bcrypt and hosting these datas in the MongoDb backend

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,13 +2,47 @@
 const express = require('express');
 // use express' router
 const router = express.Router();
-const user = require("../../db/user");
-
-
+// Mongo schema
+const User = require("../../db/User");
+// use this lib to encrypt passcode
+const bcrypt = require("bcrypt");
+// define router
 router.get("/test", (req, res) => {
-    res.json({msg:"Hi"});
+    res.json({msg:"login works"});
 });
 
+// use x-www-form-urlencoded to view the data in the postman
+router.post("/register",(req,res) => {
+    //console.log(req.body);
+    //res.json({msg:req.body});
+    User.findOne({email:req.body.email})
+        .then((user) => {
+            if(user){
+                return res.status(400).json({email:"This email has been taken"})
+            }else{
+                // create new user
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                });
+                bcrypt.genSalt(10, function (err, salt) {
+                    // 1st arg: target, 2nd arg: encrypt alg
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err)
+                            throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                                .then(user =>
+                                        res.json(user))
+                                .catch(err =>
+                                        console.log(err)
+                                );
+                    });
+                });
+            }
+        });
+});
 module.exports = router;
 
 
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 
 const connectDb =require("./db/connection");
 connectDb();
-const express = require('express');
-const path = require('path');
+const express = require("express");
 const app = express();
 const users = require("./routes/api/users");
-// Body parser middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const bodyParser = require("body-parser")
+
+// Body parser middleware, for http post header
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Member API router
-// original  ..... > new
 app.use('/api/users', users);
-// paragraph, request the file that just created 
+
 const PORT = process.env.PORT || 5000;
 
 
