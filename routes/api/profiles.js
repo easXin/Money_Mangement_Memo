ae30540eb4370d6fcdba07a303a53527dc4ac683
easXin <codingericwang@gmail.com>
2020-07-29 16:35:29 -0700

    have routers setup to retrieve user's data from the backend

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -7,6 +7,33 @@ router.get("/test", (req, res) => {
     res.json({ msg: "works...." });
 });
 
+router.post("/get", passport.authenticate("jwt", { session: false }), (req, res) => {
+    Profile.find()
+        .then(profile => {
+            if (!profile) {
+                return res.status(404).json("Access Denied");
+            }
+            return res.json(profile);
+        })
+        .catch(err => {
+            return res.status(404).json(err);
+        });
+});
+
+router.post("/:id", passport.authenticate("jwt", { session: false }), (req, res) => {
+    // param grad the data from frontend
+    Profile.findById({_id:req.params.id})
+            .then(profile =>{
+                if(!profile){
+                    return res.status(404).json("We don't have your data in our record");
+                }
+                return res.json(profile);
+            })
+            .catch(err =>{
+                return res.status(404).json(err);
+            });
+});
+
 router.post("/add",  passport.authenticate("jwt", { session: false }), (req, res) => {
     const profileField ={};
     if(req.body.type){
@@ -28,7 +55,8 @@ router.post("/add",  passport.authenticate("jwt", { session: false }), (req, res
         profileField.remark = req.body.remark;
     }
     // send to db
-    new Profile(profileField).save().then(profile =>{
+    new Profile(profileField).save()
+                .then(profile =>{
         res.json(profile);
     })
 });
