124db1ab9fb60fdf0904deeb376eec8de5127601
easXin <codingericwang@gmail.com>
2020-08-07 12:25:18 -0700

    Used element-ui to build  backend management system UI

---
+++ client/src/views/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="register">
+        <section class="form_container">
+            <div class="manage_tip">
+                <span class="title">
+                    Backend management system
+                </span>
+            </div>
+        </section>
+    </div>
+</template>
+<script>
+    export default {
+        name: "index",
+        components:{}
+    };
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/views/Register.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="index">
+        Default page
+    </div>
+</template>
+<script>
+    export default {
+        name: "index",
+        components:{}
+    };
+</script>
+
+<style>
+.register{
+    position: relative;
+    width:100px;
+    height:100px;
+    background:url('../assets/bg.jpg') no-repeat center center;
+    background-size:100% 100%;
+}
+.form_container{
+    width:370px;
+    height: 210px;
+    position:absolute;
+    top:10%;
+    left:34%;
+    padding:25px;
+    border-radius:5px;
+    text-align:center;
+
+
+}
+
+
+.form_container .manage_tip .title{
+    font-family :"Microsoft YaHei";
+    font-weight: bold;
+    font-size:26px;
+    color:#fff;
+}
+</style>
