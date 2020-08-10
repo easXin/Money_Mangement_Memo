a5d7c14ea2a42f9346cfa34a0f1a712d79bdf73c
easXin <codingericwang@gmail.com>
2020-08-10 15:51:27 -0700

    minor fix

---
+++ client/vue.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+
+    outputDir: 'dist',
+
+    lintOnSave: true,
+
+    chainWebpack: () => { },
+    configureWebpack: () => { },
+    productionSourceMap: true,
+    // css相关配置
+    css: {
+
+        extract: true,
+
+        sourceMap: false,
+
+        loaderOptions: {},
+        modules: false
+    },
+    // use thread-loader for babel & TS in production build
+    // enabled by default if the machine has more than 1 cores
+    parallel: require('os').cpus().length > 1,
+
+    pwa: {},
+    // webpack-dev-server 
+    devServer: {
+        open: process.platform === 'darwin',
+        host: '0.0.0.0',
+        port: 8081,
+        https: false,
+        hotOnly: false,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5000/api',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '/api': ''
+                }
+            }
+        },
+        before: app => { }
+    },
+
+    pluginOptions: {
+        // ...
+    }
+}
