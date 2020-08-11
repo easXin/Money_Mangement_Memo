6f8b76ef286e625c2a3955e3b99f875bb25895d6
easXin <codingericwang@gmail.com>
2020-08-10 20:00:34 -0700

    create a new branch for debugging

--- client/src/http.js
+++ client/src/http.js
@@ -1,31 +1,77 @@
-import router from './router';
+// import router from './router';
+// import axios from 'axios';
+// import {Loading,Message} from 'element-ui';
+// let loading;
+
+// function startLoading(){
+//     // options is the configuration of Loading
+//     loading = Loading.service({
+//         lock:true,
+//         text:'Loading ...',
+//         background:'rgba(0,0,0,0.7)'
+//     });
+// }
+
+// function endLoading(){
+//     loading.close();
+// }
+// // use token for router defender
+// // request dennies
+// axios.interceptors.request.use(config =>{
+//     // load animate
+//     startLoading();
+//     if(localStorage.eleToken){
+//         // set header
+//         config.headers.Authorization = localStorage.eleToken;
+//     }
+//     return config;
+// },err =>{
+//     return Promise.reject(err);
+// });
+
+// // response dennies
+// axios.interceptors.response.use(response => {
+//     endLoading();
+//     return response;
+// }, err => {
+//     endLoading();
+//     Message.error(err.response.data);
+
+//     const {status} = err.response;
+//     if(status === 401){
+//         Message.error("Token is expired , please reloading");
+//         localStorage.removeItem('eleToken');
+//         router.push('/login')
+//     }
+//     return Promise.reject(err);
+// });
+
+
+// export default axios;
+
 import axios from 'axios';
-import {Loading,Message} from 'element-ui';
+import { Loading, Message } from 'element-ui';
 let loading;
 
-function startLoading(){
+function startLoading() {
     // options is the configuration of Loading
     loading = Loading.service({
-        lock:true,
-        text:'Loading ...',
-        background:'rgba(0,0,0,0.7)'
+        lock: true,
+        text: 'Loading ...',
+        background: 'rgba(0,0,0,0.7)'
     });
 }
 
-function endLoading(){
+function endLoading() {
     loading.close();
 }
-// use token for router defender
+
 // request dennies
-axios.interceptors.request.use(config =>{
+axios.interceptors.request.use(config => {
     // load animate
     startLoading();
-    if(localStorage.eleToken){
-        // set header
-        config.headers.Authorization = localStorage.eleToken;
-    }
     return config;
-},err =>{
+}, err => {
     return Promise.reject(err);
 });
 
@@ -35,14 +81,7 @@ axios.interceptors.response.use(response => {
     return response;
 }, err => {
     endLoading();
-    Message.error(err.response.data);
-
-    const {status} = err.response;
-    if(status === 401){
-        Message.error("Token is expired , please reloading");
-        localStorage.removeItem('eleToken');
-        router.push('/login')
-    }
+    Message.error(err.response.data)
     return Promise.reject(err);
 });
 
--- client/src/router.js
+++ client/src/router.js
@@ -1,3 +1,53 @@
+// import Vue from "vue";
+// import Router from "vue-router";
+// import Index from "./views/Index.vue";
+// import Register from "./views/Register.vue";
+// import NotFound from "./views/404.vue";
+// import Login from "./views/Login.vue";
+// Vue.use(Router);
+
+// const router = new Router({
+//     mode: "history",
+//     base: process.env.BASE_URL,
+//     routes: [
+//     {
+//         path: "/",
+//         redirect: "/index"
+//     },
+//     {
+//         path: "/index",
+//         name: "index",
+//         component: Index
+//     },
+//     {
+//         path: "/register",
+//         name: "register",
+//         component: Register
+//     },
+//     {
+//         path: "*",
+//         name: "/404",
+//         component:NotFound
+//     },
+//     {
+//         path:"/login",
+//         name: "login",
+//         component: Login
+//     }
+// ],
+// });
+
+// //router defender : so that user can only user login page and register page
+// router.beforeEach((to,from,next) => {
+//     const isLogin = localStorage.eleToken ? true : false;
+//     if(to.path == "/login"|| to.path == '/register'){
+//         next();
+//     }else{
+//         isLogin? next() : next('/login')
+//     }
+// });
+// export default router;
+
 import Vue from "vue";
 import Router from "vue-router";
 import Index from "./views/Index.vue";
@@ -6,44 +56,33 @@ import NotFound from "./views/404.vue";
 import Login from "./views/Login.vue";
 Vue.use(Router);
 
-const router = new Router({
+export default new Router({
     mode: "history",
     base: process.env.BASE_URL,
     routes: [
-    {
-        path: "/",
-        redirect: "/index"
-    },
-    {
-        path: "/index",
-        name: "index",
-        component: Index
-    },
-    {
-        path: "/register",
-        name: "register",
-        component: Register
-    },
-    {
-        path: "*",
-        name: "/404",
-        component:NotFound
-    },
-    {
-        path:"/login",
-        name: "login",
-        component: Login
-    }
-],
+        {
+            path: "/",
+            redirect: "/index"
+        },
+        {
+            path: "/index",
+            name: "index",
+            component: Index
+        },
+        {
+            path: "/register",
+            name: "register",
+            component: Register
+        },
+        {
+            path: "*",
+            name: "/404",
+            component: NotFound
+        },
+        {
+            path: "/login",
+            name: "login",
+            component: Login
+        }
+    ],
 });
-
-// router defender : so that user can only user login page and register page
-router.beforeEach((to,from,next) => {
-    const isLogin = localStorage.eleToken ? true : false;
-    if(to.path == "/login"|| to.path == '/register'){
-        next();
-    }else{
-        isLogin? next() : next('/login')
-    }
-});
-export default router;
