95fa0d7e841ce4481b764d5ea0672c00aa14b756
easXin <codingericwang@gmail.com>
2020-08-11 03:19:56 -0700

    fixed bugs .....

--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import axios from 'axios';
 // ELement UI
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
@@ -10,6 +10,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
+Vue.prototype.$axios = axios;
 new Vue({
   router,
   store,
--- client/src/router.js
+++ client/src/router.js
@@ -1,34 +1,39 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Index from "./views/Index.vue";
-import Register from "./views/Register.vue";
+import Login from "./views/Login.vue";
 import NotFound from "./views/404.vue";
-//import Login from "./views/Login.vue";
+import Register from "./views/Register.vue";
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   // define all the route here
   routes: [
     {
       path: "/",
-      redirect: "/index"
+      redirect: "/index",
     },
     {
       path: "/index",
       name: "index",
-      component: Index
+      component: Index,
     },
     {
-      path:"/register",
-      name:"register",
-      component: Register
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register,
     },
     {
       path: "*",
       name: "/404",
-      component: NotFound
+      component: NotFound,
     },
-  ]
-})
+  ],
+});
